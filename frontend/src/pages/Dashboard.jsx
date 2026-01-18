79856624cb8b5085ec895b90bdc2c5f632bfcbab
VineetMelavanki <vineetmelavanki06@gmail.com>
2026-01-18 16:01:57 +0530

    Added logout feature in dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,11 +3,16 @@ import { useNavigate } from 'react-router-dom';
 export default function Dashboard()
 {
     const navigate= useNavigate();
+    const handlelogout=()=>{
+        localStorage.removeItem("token");
+        navigate("/login");
+    }
     return(
         <div>
             <h1> DASHBOARD </h1>
         <button onClick={()=>navigate("/CreateTeam")}> Create Team </button>
         <button onClick={()=>navigate("/allteams")}> Total Teams </button>
+        <button onClick={handlelogout}>Logout</button>
         </div>
     );
 }
