a3a10505b313cb14f55264f34a8ef338cadba7b3
VineetMelavanki <vineetmelavanki06@gmail.com>
2026-01-21 23:56:19 +0530

    Frontend : Added dashboard Toolbar including profile logo and Notification

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,53 +1,29 @@
 import React from "react";
-import {useNavigate} from "react-router-dom"
-import {Box,Grid,Typography,Card, Icon,Container, CardActions,CardContent,Button} from "@mui/material"
-import{Add,Description,LogoutIcon} from "@mui/icons-material"
+
+import{AppBar,Toolbar,Box,Typography,IconButton}from "@mui/material"
+import {Notifications,AccountCircle} from "@mui/icons-material"
 export default function Dashboard()
 {
-    const handlelogout=()=>{
-        localStorage.removeItem("token");
-        navigate("/login");
-    }
-    const navigate=useNavigate();
-    const cards=[
-        {
-            title:"Create team",
-            Description:"Create your own team here",
-            icon :<Add sx={{fontSize:60,color:"secondary.main"}}/>,
-            action:()=>navigate("/Create-team"),
-            Textbtn:"Create team",
-        },
-        {
-            title:"Logout",
-            Description:"Log out from here",
-            icon:<LogoutIcon sx={{fontSize:60,color:"secondary.main"}}/>,
-            action:()=>{handlelogout},
-            Textbtn:"Logout",
-        },
-    ];
-
     return(
-        <Container maxWidth="lg">
-            <Box sx={{mt:4,mb:4}}>
-                <Typography variant="h1" sx={{color:"gray"}}>Collab Genius</Typography>
-                <Typography variant="h1" sx={{color:"gray"}}>Your Project Management Dashboard</Typography>
-                <Grid container spacing={4}>
-                    {cards.map((card,index)=>(
-                        <Grid item xs={12} sm={6} md={4} key={index}>
-                            <Card sx={{display:"flex",height:"100%", flexDirection:"column"}}>
-                                <CardContent sx={{textAlign:"center"}}>
-                                 {card.icon}
-                                 <Typography variant="h5" sx={{mt:2}}>{card.title}</Typography>
-                                 <Typography variant="body2">{card.Description}</Typography>
-                                </CardContent>
-                                <CardActions>
-                                    <Button variant="outlined" sx={{color:"gray"}} onClick={card.action}>{card.Textbtn}</Button>
-                                </CardActions>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid>
-            </Box>
-        </Container>
+        <AppBar position="fixed" color="primary">
+            <Toolbar>
+                <Box>
+                    <Typography variant="h6">Collab-Genius</Typography>
+                </Box>
+                <Box sx={{flexGrow:1,textAlign:"center"}}>
+                    <Typography variant="subtitle1">
+                        DashBoard
+                    </Typography>
+                </Box>
+                <Box sx={{display:"flex", gap:1}}>
+                <IconButton>
+                    <Notifications/>
+                </IconButton>
+                <IconButton>
+                    <AccountCircle/>
+                </IconButton>
+                </Box>
+            </Toolbar>
+        </AppBar>
     )
 }
