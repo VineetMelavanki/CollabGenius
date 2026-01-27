4f8d8c2f08d5ef2113a51ad39de3db7b29795f18
VineetMelavanki <vineetmelavanki06@gmail.com>
2026-01-27 22:45:33 +0530

    Frontend: Added Create Team and view Profile in dasboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,39 @@
 
-import{Box,Typography}from "@mui/material"
+import{Button,Box,Card,CardActions,CardContent,Typography}from "@mui/material"
+import{Add, Celebration} from "@mui/icons-material"
+import VisibilityIcon from "@mui/icons-material/Visibility"
+import { useNavigate } from "react-router-dom";
 export default function Dashboard() {
+  const navigate=useNavigate();
   return (
-    <Box sx={{height:"100vh",width:"100vw",justifyContent:"center",alignItems:"normal",display:"flex"}}>
-      <Typography variant="h3" color="black">
-        Hello Welcome to my Dashboard
-      </Typography>
+    <Box sx={{flexWrap:"wrap",display:'flex',gap:2}}>
+      <Card sx={{width:250}}>
+       <CardContent sx={{justifyContent:"center",textAlign:"center"}}>
+        <Add/>
+        <Typography sx={{mt:1}}>
+          Create your Team
+        </Typography>
+        <CardActions sx={{justifyContent:"center",textAlign:"center"}}>
+          <Button sx={{color:"red"}} onClick={()=>navigate("/CreateTeam")}>
+           Create
+          </Button>
+        </CardActions>
+       </CardContent>
+      </Card>
+      <Card sx={{width:250}}>
+       <CardContent sx={{justifyContent:"center",textAlign:"center"}}>
+        <VisibilityIcon/>
+        <Typography sx={{mt:1}}>
+         View your profile 
+        </Typography>
+       
+       <CardActions sx={{justifyContent:'center',alignItems:"center"}}>
+        <Button sx={{color:"red"}} onClick={()=>navigate("/View-profile")}>
+          View
+          </Button>
+       </CardActions>
+       </CardContent>
+      </Card>
     </Box>
   );
 }
