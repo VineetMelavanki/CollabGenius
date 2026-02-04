c99642c3c34e08978b1c472e181f33f6dd1bb44a
VineetMelavanki <vineetmelavanki06@gmail.com>
2026-02-04 21:22:56 +0530

    Frontend : Added about page to give a brief description of the project

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,19 +1,60 @@
 import React from "react";
+import HoverEfffect from "../effects/HoverCard";
 import {Box,Paper,Typography,Card,Grid,CardContent,CardActions} from "@mui/material"
 export default function About()
 {
     return(
-        <Box sx={{display:"flex",minWidth:"100vw"}}>
-            <Grid container spacing={3}>
-             <Grid item xs={12} sm={6} md={3}>
-                <Card sx={{width:250,height:250}}>
-                    <CardContent>
-                        <Typography>hello</Typography>
-                    </CardContent>
-
-                </Card>
-             </Grid>
-            </Grid>
+        
+         <Box sx={{display:"flex",minWidth:"100vw",minHeight:"100vh",flexDirection:"column",color:"black"}}>
+           <Box sx={{textAlign:"center",mb:3}}>
+           <Typography variant="h3"sx={{mb:2}}>
+            About our platform
+           </Typography>
+            <Typography variant="h6" sx={{color:"black"}}>
+            Our platform provides an easy way to collaborate and maintain transparency , this platform is user friendly and can be used in wide range of professions
+            </Typography>
+           </Box>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={4}>
+            <HoverEfffect>
+             <Paper sx={{
+                width:400,
+                p:"2rem 2rem",
+                justifyContent:"center",
+                textAlign:'center'
+            }}>
+                
+                 <Typography variant="h6" sx={{color:"red",mb:2}}>
+                Revenue generate 
+             </Typography>
+             <Typography variant="h7" sx={{color:"red"}}>
+               $300K+
+             </Typography>
+            </Paper>
+            </HoverEfffect>
+            
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <HoverEfffect>
+              <Paper sx={{
+                width:400,
+                p:"2rem 2rem",
+                justifyContent:"center",
+                textAlign:'center'
+            }}>
+            <Typography variant="h6" sx={{color:"red",mb:2}}>
+                Users Attracted
+            </Typography>
+            <Typography>
+                Total Usera :100k
+            </Typography>
+            </Paper>  
+            </HoverEfffect>
+            
+          </Grid>
+        </Grid>
         </Box>
+         
+        
     )
 }
