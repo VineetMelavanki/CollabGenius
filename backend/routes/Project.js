adeb396539f64b0ff4b9d74e63988eedd39403fb
VineetMelavanki <vineetmelavanki06@gmail.com>
2026-02-20 22:27:35 +0530

    Backend : created Routes to add members

--- backend/routes/Project.js
+++ backend/routes/Project.js
@@ -1,9 +1,11 @@
 const express =require("express");
-const {CreateProject,Getallprojects,getprojectbyId,deleteproject}=require("../controllers/Project");
+const {CreateProject,Getallprojects,getprojectbyId,deleteproject,yourprojects,addmembers}=require("../controllers/Project");
 const {authmiddleware} =require("../middleware/authmiddleware")
 const ProjectRouter = express.Router();
 ProjectRouter.post("/Create-Project",authmiddleware,CreateProject);
 ProjectRouter.get("/getallprojects",authmiddleware,Getallprojects);
 ProjectRouter.delete("/delete/:id",authmiddleware,deleteproject);
 ProjectRouter.get("/get-project/:id",authmiddleware,getprojectbyId);
+ProjectRouter.get("/your-projects",authmiddleware,yourprojects);
+ProjectRouter.post("/Add-members/:id",authmiddleware,addmembers);
 module.exports=ProjectRouter;
