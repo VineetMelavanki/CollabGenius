405e2571daadcae32e2ac7a76bfdad510af53e78
VineetMelavanki <vineetmelavanki06@gmail.com>
2026-02-20 22:26:59 +0530

    Backend : Added a feature to add member in a project

--- backend/controllers/Project.js
+++ backend/controllers/Project.js
@@ -15,9 +15,11 @@ async function CreateProject(req,res)
     {
         return res.status(409).json({msg : "Project Title already exists ",success : false});
     }
+    const ownerId=req.user.id;
     const Newproject= await Project.create({
         title,
-        ownerId:req.user.id,
+        ownerId:ownerId,
+        members:[ownerId],
         status,
         createdAt,
         updatedate,
@@ -41,15 +43,76 @@ async function Getallprojects(req,res)
         return res.status(500).json({msg : "Internal server error ",success : false, error : error.message});
     }
 }
-
+async function addmembers(req,res)
+{
+    try{
+        const userId=req.user.id;
+       const {id}=req.params;
+       const{email}=req.body;
+       
+       const Projectexists=await Project.findById(id)
+       .populate("members","name email");
+       if(!Projectexists)
+       {
+        return res.status(404).json({msg:"Project not found",success:false});
+       }
+       
+      if(Projectexists.ownerId.toString()!==userId)
+      {
+        return res.status(401).json({msg:"Only owners can add members",success:false});
+      }
+      const user=await User.findOne({email});
+      if(!user)
+      {
+        return res.status(404).json({msg:"User not found",success:false});
+      }
+      if(Projectexists.members.some(
+        member=>member._id.toString()===user._id.toString()
+      ))
+      {
+        return res.status(409).json({msg:"user already a member",success:false});
+      }
+      Projectexists.members.push(user._id);
+      await Projectexists.save();
+      console.log("Updated Project is : ",Projectexists);
+      console.log("Project members : ",Projectexists.members);
+      return res.status(201).json({msg:"User added successfully to the team",Project:Projectexists,success:true})
+    }catch(error)
+    {
+        return res.status(500).json({msg:"Internal server error",success:false});
+    }
+}
+async function yourprojects(req,res)
+{
+    try{
+        const id=req.user.id;
+        const yourproject=await Project.find({
+            ownerId:id,
+        });
+        if(yourproject.length===0)
+        {
+            return res.status(404).json({msg:"You havent created any project",success:false});
+        }
+        console.log(id);
+        console.log("Your projects are : ",yourproject);
+        return res.status(200).json({msg:"Project fetched successfully",myproject:yourproject,success:true});
+    }catch(error)
+    {
+        console.log(error);
+        return res.status(500).json({msg:"Interal server error",success:false});
+    }
+}
 async function getprojectbyId(req,res){
     try{
        const {id}=req.params;
-       const projectinfo=await Project.findById(id);
+       const projectinfo=await Project.findById(id)
+       .populate("members","name email")
+       .populate("ownerId","name email");
        if(!projectinfo)
        {
         return res.status(404).json({msg:"Project Not found",success:false});
        }
+       
        return res.status(200).json({msg:"Project found",projectdata:projectinfo,success:true});
     }catch(error)
     {
@@ -76,4 +139,4 @@ async function deleteproject(req,res)
         return res.status(500).json({msg:"Internal server error",success:false});
     }
 }
-module.exports={CreateProject,Getallprojects,getprojectbyId,deleteproject};
+module.exports={CreateProject,Getallprojects,getprojectbyId,deleteproject,yourprojects,addmembers};
