e1ce3eb6ea75f15029c118703ec45fa4481fe0a2
VineetMelavanki <vineetmelavanki06@gmail.com>
2026-02-24 22:43:30 +0530

    Backend : implemented routes for the searchby title controller

--- backend/routes/Project.js
+++ backend/routes/Project.js
@@ -1,5 +1,5 @@
 const express =require("express");
-const {CreateProject,Getallprojects,getprojectbyId,deleteproject,yourprojects,addmembers,removemember,getprojectByname}=require("../controllers/Project");
+const {CreateProject,Getallprojects,getprojectbyId,deleteproject,yourprojects,addmembers,removemember,getprojectBytitle}=require("../controllers/Project");
 const {authmiddleware} =require("../middleware/authmiddleware")
 const ProjectRouter = express.Router();
 ProjectRouter.post("/Create-Project",authmiddleware,CreateProject);
@@ -7,7 +7,7 @@ ProjectRouter.get("/getallprojects",authmiddleware,Getallprojects);
 ProjectRouter.delete("/delete/:id",authmiddleware,deleteproject);
 ProjectRouter.get("/get-project/:id",authmiddleware,getprojectbyId);
 ProjectRouter.get("/your-projects",authmiddleware,yourprojects);
-ProjectRouter.get("/get-project-by-title",authmiddleware,getprojectByname);
+ProjectRouter.get("/get-project-by-title",authmiddleware,getprojectBytitle);
 ProjectRouter.post("/Add-members/:id",authmiddleware,addmembers);
 ProjectRouter.delete("/remove-member/:projectId/:memberId",authmiddleware,removemember);
 module.exports=ProjectRouter;
