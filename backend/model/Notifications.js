b1ab877306d12724636bf6235a7188a232a87d64
VineetMelavanki <vineetmelavanki06@gmail.com>
2026-03-04 19:45:13 +0530

    Backend : Added controller for notification section

---
+++ backend/model/Notifications.js
@@ -0,0 +1,31 @@
+const mongoose=require("mongoose");
+
+const NotificationSchema=new mongoose.Schema({
+    receiver:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    sender:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    message:{
+        type:String,
+        required:true,
+    },
+    status:{
+        type:String,
+        enum:["accepted","pending","declined"],
+        default:"pending",
+    },
+    project:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"project",
+        required:true,
+    }
+});
+const Notifications=mongoose.model("Notification",NotificationSchema);
+
+module.exports={Notifications};
