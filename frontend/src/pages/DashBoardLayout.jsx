a16396220b9f40bfb09997e79e577df7b5845d70
VineetMelavanki <vineetmelavanki06@gmail.com>
2026-05-10 15:17:28 +0530

    Frontend : Altered the size of icons in dashboard tab and make it more interactive

--- frontend/src/pages/DashBoardLayout.jsx
+++ frontend/src/pages/DashBoardLayout.jsx
@@ -11,6 +11,7 @@ export default function DashBoardLayout({ children }) {
   const[error,seterror]=useState("");
   const[message,setmessage]=useState("");
   const token=localStorage.getItem("token");
+  const[darkmode,setdarkmode]=useState(false);
   const[about,setabout]=useState(true);
   useEffect(() => {
     const profileverify = async () => {
@@ -122,8 +123,8 @@ export default function DashBoardLayout({ children }) {
       <div className="flex-1 flex flex-col">
         {/* Header */}
         <header className="bg-white shadow-sm sticky top-0 z-30">
-          <div className="flex items-center justify-between px-6 py-4">
-            <div className="flex items-center gap-4">
+          <div className="flex items-center justify-between p-2">
+            <div className="flex items-center gap-3">
               <button onClick={()=>{navigate(-1);setabout(!about)}}
                 className="flex items-center gap-2 text-blue-600 hover:text-blue-400 font-medium border-">
                 <IoArrowBack size={20}/>
@@ -132,16 +133,18 @@ export default function DashBoardLayout({ children }) {
                 onClick={() => setopen(true)}
                 className="p-2 hover:bg-gray-100 rounded-lg transition"
               >
+                
                 <svg className="w-6 h-6 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
               </button>
-              <h1 className="text-xl font-bold text-gray-800">Collab Genius</h1>
+              <h1 className="text-lg font-bold text-gray-800">Collab Genius</h1>
             </div>
             
-            <h2 className="text-2xl font-bold text-primary-600 hidden md:block">Dashboard</h2>
+            <h2 className="text-lg font-bold text-primary-600 hidden md:block">Dashboard</h2>
             
-            <div className="flex gap-3">
+            <div className="flex gap-3 ">
+              <button onClick={()=>setdarkmode(!darkmode)} className="bg-black flex sm:text-sm my-2  md:text-md mx-3 lg:text-lg  text-white  font-bold border border-black p-2 rounded-2xl">Dark Mode</button>
               <button className="relative p-2 rounded-full hover:bg-gray-200 transition" onClick={()=>navigate("/Notifications")}>
                 <BellIcon className="h-6 w-6 text-gray-700" />
                 {Notifications.length > 0 && (
@@ -150,7 +153,7 @@ export default function DashBoardLayout({ children }) {
                   </span>
                 )}
               </button>
-              <button className="text-blue-500 border-2 border-gray-300 font-medium px-4 py-2 rounded-lg hover:bg-gray-50 transition"
+              <button className="text-blue-500 border-2  border-gray-300 font-medium px-4  rounded-lg hover:bg-gray-50 transition"
               onClick={()=>navigate("/allusers")}>
                 All USERS
               </button>
@@ -170,9 +173,16 @@ export default function DashBoardLayout({ children }) {
         </header>
 
         {/* Page Content */}
-        <div className="min-h-screen w-full">
+      {darkmode && <div className=" bg-black/75 min-h-screen w-full">
+           <div className="pt-4 px-4">
           {children}
         </div>
+        </div> }
+      {!darkmode && <div className=" bg-white min-h-screen w-full">
+        <div className="pt-4 px-4">
+          {children}
+        </div>
+        </div> }  
       </div>
     </div>
   );
