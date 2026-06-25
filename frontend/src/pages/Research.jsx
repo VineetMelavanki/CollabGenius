720aeebac278889f03f44770fb9a0ee0982ea5ad
VineetMelavanki <vineetmelavanki06@gmail.com>
2026-06-25 15:49:06 +0530

    Frontend : Made suitable changes and code tranfer to the CreatedRepos.jsx

--- frontend/src/pages/Research.jsx
+++ frontend/src/pages/Research.jsx
@@ -6,6 +6,7 @@ import { NotebookPen ,UserRound ,ChevronUp,ChevronDown} from "lucide-react";
 import { MagnifyingGlassIcon,FolderOpenIcon ,PlusIcon ,ChatBubbleOvalLeftIcon,ChevronDoubleDownIcon,ChevronDoubleUpIcon} from "@heroicons/react/24/outline";
 import ResearchSearchBar from "../Components/ResearchSearchBar";
 import SavedGithubRepos from "../Components/SavedGithubrepos";
+import CreatedRepos from "../Components/CreatedRepos";
 import nulllogo from  "../assets/logos/null.png"
 import{FaTrash,FaPencilAlt,FaUser} from "react-icons/fa"
 import { useNavigate } from "react-router-dom";
@@ -13,30 +14,16 @@ export default function Research() {
   const navigate=useNavigate();
   const { workId, projectId } = useParams();
   const [projectName, setprojectName] = useState(null);
-  const [msg, setmsg] = useState("");
-  const [error, seterror] = useState("");
-
   const[memberslist,setmemberslist]=useState(null);
   const[allmembers,setallmembers]=useState([]);
   const[Leader,setLeader]=useState(null);
   const[choosesavedrepo,setchoosesavedrepo]=useState(false);
   const[taskarrowdown,settaskarrowdown]=useState(true);
   const[relatedrepolists,setrelatedrepolist]=useState([]);
- 
-
   const[opentask,setopentask]=useState(null);
   const[openmembers,setopenmembers]=useState(null);
- 
   const[foldersection,setfoldersection]=useState(false);
-
    const[SavedRepos,setSaveRepos]=useState([]);
-  const[repolist,setrepolist]=useState([]);
-  const[gitinfo,setgetinfo]=useState({
-    name:"",
-    description:"",
-    private:false,
-  });
-  
   const[Taskedit,setTaskedit]=useState(false);
   const[ResearchTaskformdata,setResearchTaskformdata]=useState({
     description:"",
@@ -45,9 +32,7 @@ export default function Research() {
   const[isSaved,setisSaved]=useState(false);
 
 
-  const handlegitinfo=(e)=>{
-    setgetinfo((prev)=>({...prev,[e.target.name]:e.target.value}));
-  }
+  
   useEffect(()=>{
     const verifyowner=async()=>{
          try{
@@ -69,53 +54,8 @@ export default function Research() {
     } 
     verifyowner();
   },[workId,projectId])
-  useEffect(()=>{
-     const getallRepo=async()=>{
-        seterror("");
-        setmsg("");
-        try{
-          const response=await axios.get(`http://localhost:8000/api/User/github/get-repo/${projectId}/${workId}`,
-            {
-              withCredentials:true,
-            }
-          );
-          setrepolist(response.data.allrepo);
-         
-        }catch(error)
-        {
-           if(error.response)
-           {
-            seterror(error.response?.data?.msg || "Cannot fetch repositories");
-           }
-           else
-           {
-            seterror("Internal server error");
-           }
-        }
-     }
-     getallRepo();
-  },[workId,projectId])
-  const handlereposubmit=async(e)=>{
-    e.preventDefault();
-    seterror("");
-    setmsg("");
-    try{
-        const response=await axios.post(`http://localhost:8000/api/User/github/create-repo/${projectId}/${workId}`,gitinfo,
-          {
-            withCredentials:true,
-          }
-        );
-       setrepolist((prev)=>[...prev,response.data.repo]);
-       setmsg(response.data?.msg || "Github repository created successfully");
-    }catch(error)
-    {
-        if(error.response)
-        {
-          seterror(error.response?.data?.msg || "Cannot create github Repo");
-        }
-    }
-  }
-  const[repooption,setrepooption]=useState(false);
+  
+
   const[researchhub,setresearchhub]=useState(false);
   useEffect(() => {
     
@@ -280,41 +220,7 @@ export default function Research() {
   }
   return (
     <div className="flex w-full  rounded-2xl">
-       {repooption && 
-           <div onClick={()=>setrepooption(false)} className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4">
-           
-              <div className="bg-white rounded-2xl shadow-xl w-full max-w-md p-8"
-               onClick={(e) => e.stopPropagation()}>
-                 
-                <div className="flex flex-col gap-3 justify-center items-center">
-                  <div className="flex items-center justify-center font-mono ">
-                      <h1 className="text-lg">Create your own github repo</h1>
-                  </div>
-                  {msg && <p className=" text-green-600 font-mono text-md">{msg}</p>}
-            {error && <p className=" text-red-600 font-mono text-md">{error}</p>}
-                  <form onSubmit={handlereposubmit} className="flex flex-col gap-3">
-                    <input type="text"
-                    name="name"
-                    value={gitinfo.name}
-                    placeholder="Enter repository name"
-                    onChange={handlegitinfo}
-                    className="text-md font-bold  border-purple-300 border-2 hover:border-purple-600 p-3 rounded-2xl"
-                    />
-                    <input type="text"
-                    name="description"
-                    value={gitinfo.description}
-                    placeholder="Enter description"
-                    onChange={handlegitinfo}
-                    className="text-md font-bold  border-purple-300 border-2 hover:border-purple-600 p-3 rounded-2xl"/>
-                    <button type="button" onClick={()=>setgetinfo((prev)=>({...prev,private:!prev.private}))}
-                      className={`text-white font-bold rounded-2xl p-3 transition-all duration-300 ${gitinfo.private ? "bg-red-500 hover:bg-red-600" : "bg-green-500 hover:bg-green-600"}`}>
-                        {gitinfo.private ? "Change to public": "Change to private"}
-                      </button>
-                    <button disabled={!gitinfo.name.trim() || !gitinfo.description.trim()} type="submit" className="font-bold text-white bg-red-500 hover:bg-red-600 text-xl rounded-xl p-3">Create</button>
-                  </form>
-                </div>
-              </div>
-            </div>}
+       
             {foldersection && !researchhub &&  (
               <SavedGithubRepos
               projectId={projectId}
@@ -358,44 +264,11 @@ export default function Research() {
           onClose={()=>setresearchhub(false)}/>
          )}
         </div>
-         <div className="flex bg-gray-100 flex-col gap-3 w-full h-full rounded-2xl">
-          <div className="flex flex-col gap-2 p-3">
-            <div className="flex flex-row gap-3">
-              <h1 className="text-lg p-3 font-mono">GITHUB repositories </h1>
-              <div className="flex flex-1 justify-end">
-              {Leader && 
-                <button onClick={()=>setrepooption(true)} className="text-sm font-bold text-red-500   rounded-2xl p-2 hover:text-red-600">
-                  <PlusIcon className="text-red h-6 w-6"/>
-                </button>}  
-              </div>
-            </div>
-            {repolist.length===0 ?(
-            <div className="text-red-200 font-mono text-lg">No respositories created</div>
-          ) :(
-            <div className="grid grid-col-1 gap-3">
-              {repolist.map((repo)=>(
-                <div key={repo._id}  className="flex flex-col  bg-white p-4 rounded-xl shadow hover:-translate-y-1 transition-all duration-200">
-                      <div className="flex flex-row ">
-                        <h1 className="text-blue-500 font-bold">{repo.name}</h1>
-                         
-                        <div className="flex flex-1 justify-end mx-4">
-                          <a href={repo.repourl}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-blue-600 hover:text-blue-700 mt-4 inline-flex items-center gap-2 font-semibold " >
-                            View
-                          </a>
-                        </div>
-                      </div>
-                      <p className="text-md font-thin">{repo.description}</p>
-                </div>
-              ))}
-            </div>
-          )
-          }
-          </div>
-          
-         </div>
+         <CreatedRepos
+         projectId={projectId}
+         workId={workId}
+         Leader={Leader}
+         />
          <div className="flex bg-gray-100 flex-col gap-3 w-full h-full rounded-2xl">
           <div className="flex flex-col gap-3 p-5">
             <div className="flex flex-row gap-2">
