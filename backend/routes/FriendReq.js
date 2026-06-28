e37a3328bcf90828081f850028178224d9942d54
VineetMelavanki <vineetmelavanki06@gmail.com>
2026-06-28 18:23:46 +0530

    Backend : Added routes to send requests accept requests decline requests and fetch requests so that it can be used by frontend to call backend

---
+++ backend/routes/FriendReq.js
@@ -0,0 +1,9 @@
+const express=require("express");
+const FriendRequestRouter=new express.Router();
+const {SendFriendRequest,fetchAllfriendRequests,acceptFriendRequest,declineFriendRequest}=require("../controllers/FriendRequest")
+const {authmiddleware} =require("../middleware/authmiddleware")
+FriendRequestRouter.post("/send-request/:profileId/:receiverId",authmiddleware,SendFriendRequest);
+FriendRequestRouter.get("/get-all-requests",authmiddleware,fetchAllfriendRequests);
+FriendRequestRouter.post("/accept-request",authmiddleware,acceptFriendRequest);
+FriendRequestRouter.post("/decline-request",authmiddleware,declineFriendRequest);
+module.exports=FriendRequestRouter;
