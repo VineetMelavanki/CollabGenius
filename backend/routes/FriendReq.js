63cfb9c38c4e6a15200b554de9f26e34cf729069
VineetMelavanki <vineetmelavanki06@gmail.com>
2026-06-29 23:17:00 +0530

    Backend :Added routes for profile verifications

--- backend/routes/FriendReq.js
+++ backend/routes/FriendReq.js
@@ -1,9 +1,10 @@
 const express=require("express");
 const FriendRequestRouter=new express.Router();
-const {SendFriendRequest,fetchAllfriendRequests,acceptFriendRequest,declineFriendRequest}=require("../controllers/FriendRequest")
+const {SendFriendRequest,fetchAllfriendRequests,acceptFriendRequest,declineFriendRequest,checkrequests}=require("../controllers/FriendRequest")
 const {authmiddleware} =require("../middleware/authmiddleware")
 FriendRequestRouter.post("/send-request/:profileId/:receiverId",authmiddleware,SendFriendRequest);
 FriendRequestRouter.get("/get-all-requests",authmiddleware,fetchAllfriendRequests);
 FriendRequestRouter.post("/accept-request/:requestId",authmiddleware,acceptFriendRequest);
 FriendRequestRouter.post("/decline-request/:requestId",authmiddleware,declineFriendRequest);
+FriendRequestRouter.post("/check-requests",authmiddleware,checkrequests);
 module.exports=FriendRequestRouter;
