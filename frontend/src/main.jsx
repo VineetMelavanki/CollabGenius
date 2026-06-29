e3c2052251437d47082b37a845eb519948805973
VineetMelavanki <vineetmelavanki06@gmail.com>
2026-06-29 17:15:13 +0530

    Frontend : Wrapped App.jsx inside AuthProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,14 @@ import App from './App.jsx'
 import {GoogleOAuthProvider} from "@react-oauth/google"
 import {OAuthifyProvider} from "oauthify"
 import { Meta } from 'react-router-dom'
+import { AuthProvider } from './AuthContext.jsx'
 const CLIENT_ID="659066824406-8clgik6i16ef14pa8i920svco5m2l6vb.apps.googleusercontent.com"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-        <GoogleOAuthProvider clientId={CLIENT_ID}>
-        <App />
-        </GoogleOAuthProvider>
+          <AuthProvider>
+            <GoogleOAuthProvider clientId={CLIENT_ID}>
+            <App />
+            </GoogleOAuthProvider>
+          </AuthProvider>
   </StrictMode>,
 )
