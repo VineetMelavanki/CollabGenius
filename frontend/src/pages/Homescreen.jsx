895ec25f3ac5d6f6dfcd25fafb59ebe6d7569523
VineetMelavanki <vineetmelavanki06@gmail.com>
2026-07-03 00:32:54 +0530

    Frontend : Written handlesubmit and handlechange function to fetch reseult when a prompt is entered

--- frontend/src/pages/Homescreen.jsx
+++ frontend/src/pages/Homescreen.jsx
@@ -4,13 +4,45 @@ import { FaUserFriends } from "react-icons/fa";
 import { FaClock } from "react-icons/fa";
 import { FolderIcon } from "@heroicons/react/24/outline";
 import axios from "axios";
+import OllamaDashboard from "../Components/AIdashboard/Ollamadashboard";
+import {CpuChipIcon} from "@heroicons/react/24/outline";
 export default function Homescreen(){
    const {user, loading}=useAuth();
    const[users,setusers]=useState(0);
    const[projects,setprojects]=useState([]);
    const[researchs,setresearchs]=useState({});
-   
+   const[openaisearch,setopenaisearch]=useState(false);
    const[Teams,setTeams]=useState(0);
+   const[prompt,setprompt]=useState("");
+   const[answer,setanswer]=useState([]);
+   const handlechange=(e)=>{
+    setprompt(e.target.value);
+   }
+   const handlesubmit=async(e)=>{
+    e.preventDefault();
+  
+    try{
+       const response=await axios.post("http://localhost:8000/api/ai/get-answers/",{
+        prompt,
+       },{
+        withCredentials:true,
+       });
+      setanswer(response.data?.Profiles);
+      console.log("The answer are : ",response.data?.Profiles);
+
+    }catch(error)
+    { 
+       if(error.response)
+       {
+        alert(error.response?.data?.msg || "Cannot fetch answers");
+       }
+       else
+       {
+        console.log("The error is : ",error);
+        alert("Internal server error");
+       }
+    }
+   }
    useEffect(()=>{
     const fetchallprojects=async()=>{
       try{
@@ -95,20 +127,33 @@ export default function Homescreen(){
 
     return(
         <div className="min-h-screen w-full">
+          {openaisearch && <OllamaDashboard
+          onClose={()=>setopenaisearch(false)}
+          prompt={prompt}
+          answer={answer}/>
+          }
           <div className="flex flex-col gap-2 w-full border p-2">
              <div className="flex sm:flex-col lg:flex-row gap-2 border  p-2">
                <div className="flex flex-col border sm:w-full lg:w-1/2">
-                 <h1 className="text-3xl font-bold">WELCOME BACK, <span className="text-purple-500">{user?.name}</span> !</h1>
-                 <h1 className="text-gray-500 text-md mt-1">Let's build something amazing today</h1>
+                 <div className="flex flex-row gap-2 ">
+                   <h1 className="text-3xl font-bold">WELCOME BACK, <span className="text-purple-500">{user?.name}</span> !</h1>
+                   <div className="flex flex-1 justify-end p-2">
+                     <CpuChipIcon onClick={()=>setopenaisearch(true)} className="w-8 h-8 hover:text-red-500"/>
+                   </div>
+                  </div>
+                   <h1 className="text-gray-500 text-md">Let's build something amazing today</h1>
+                 
                  <div className="flex flex-col p-4 rounded-2xl mt-8 mx-4 border bg-gradient-to-r from-slate-50 via-slate-100 to-slate-50">
                    <h1 className="text-black font-bold mb-2">What do you want to build today?</h1>
-                   <form className="max-w-lg">
+                   <form onSubmit={handlesubmit} className="max-w-lg">
                     <div className="flex items-center gap-3">
                       <input type="text"
+                      value={prompt}
+                      onChange={handlechange}
                       placeholder="Describe your ideas , project or what you need"
                         className="flex-1 w-full h-12 px-4 rounded-xl border border-gray-300 outline-none focus:ring-2 focus:ring-purple-500"/>
 
-                          <button type="submit"
+                          <button type="submit" onClick={()=>setopenaisearch(true)}
                             className="h-12 px-6 bg-purple-600 text-white rounded-xl hover:bg-purple-700 transition"
                           >
                            ✨ Ask AI
