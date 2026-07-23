1618875b5dd01774f4b7ac61f5d1a6078140a491
VineetMelavanki <vineetmelavanki06@gmail.com>
2026-07-23 22:50:41 +0530

    Backend : Added routes to fetch chat history

--- backend/routes/Chat.js
+++ backend/routes/Chat.js
@@ -1,8 +1,9 @@
 const express=require("express");
 const { authmiddleware } = require("../middleware/authmiddleware");
-const { CreateChat, getChatbyId ,getalluserchats} = require("../controllers/Chat");
+const { CreateChat, getChatbyId ,getalluserchats,getchatmessages} = require("../controllers/Chat");
 const ChatRouter=new express.Router();
 ChatRouter.post("/create-chat",authmiddleware,CreateChat);
 ChatRouter.get("/get-chat-by-id",authmiddleware,getChatbyId);
 ChatRouter.get("/get-all-user-chats/:userId",authmiddleware,getalluserchats);
+ChatRouter.get("/get-chat-messages/:chatId",authmiddleware,getchatmessages);
 module.exports=ChatRouter;
